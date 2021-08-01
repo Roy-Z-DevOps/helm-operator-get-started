f855dfa162045e866eb88aa9ed9207a07908a704
Your Name <you@example.com>
2021-08-01 15:59:28 +0300

    ci script

---
+++ hack/ci-echo.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+repository="gcr.io/echoapp-320414/echoapp"
+branch="master"
+version=""
+commit=$(cat /dev/urandom | env LC_CTYPE=C tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1 | awk '{print tolower($0)}')
+
+while getopts :r:b:v: o; do
+    case "${o}" in
+        r)
+            repository=${OPTARG}
+            ;;
+        b)
+            branch=${OPTARG}
+            ;;
+        v)
+            version=${OPTARG}
+            ;;
+    esac
+done
+shift $((OPTIND-1))
+
+if [ -z "${version}" ]; then
+    image="${repository}:${branch}-${commit}"
+    version="0.4.0"
+else
+    image="${repository}:${version}"
+fi
+
+echo ">>>> Building image ${image} <<<<"
+
+docker build -t echoapp -f $PWD/echo/Dockerfile .
+
+docker tag echoapp ${image}
+
+docker push ${image}
+
